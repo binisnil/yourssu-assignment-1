cfafd880d28277e6d3f00fdb70bbee1c365bfea9
binisnil <dbsqlssss0704@gmail.com>
2023-09-27 16:47:11 +0900

    [Setting] 초기 파일 세팅

---
+++ yourssu-assignment-1/yourssu-assignment-1/yourssu_assignment_1App.swift
@@ -0,0 +1,17 @@
+//
+//  yourssu_assignment_1App.swift
+//  yourssu-assignment-1
+//
+//  Created by 박윤빈 on 2023/09/27.
+//
+
+import SwiftUI
+
+@main
+struct yourssu_assignment_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
